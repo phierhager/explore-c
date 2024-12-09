a60cce0c9da22b3d9e0f8e7e07b9e6785140901c
Philip Hierhager <philip.hierhager@driveblocks.ai>
2024-12-09 10:58:15 +0100

    first part of socket implementation

---
+++ networking/socket.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define SERVER_PORT 12345
+#define MAX_LINE 256
+
+int main(int argc, char * argv[]) {
+    FILE *fp;
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host;
+    char buf[MAX_LINE];
+    int s;
+    int len;
+
+    if (argc!=2) {
+        fprintf(stderr, "usage: simplex-talk host\n");
+    }
+
+    host = argv[1];
+
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if (!hp) {
+        fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+        exit(1);
+    }
+
+    /* build the address data structure */
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* if active open, connect to the server */
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("simplex-talk: socket");
+        exit(1);
+    }
